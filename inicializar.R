cc17270641a90801420189fdbf1345ca659b4154
gusmmm <jgustavomartins@gmail.com>
2016-08-05 14:29:49 +0100

    definiçao dos directorios e ficheiros a usar

---
+++ inicializar.R
@@ -0,0 +1,24 @@
+### ficheiro com as intrucçoes para iniciar o projecto ###
+
+### packages adicionais ###
+
+library(stringr)
+library(lubridate)
+library(readxl)
+library(dplyr)
+
+
+### directorios do projecto ###
+
+# directorio principal -dp
+dp <- getwd()
+# directorio dos dados originais - dd
+dd <- paste0(dp,"/dados/")
+# directorio de output - do
+do <- paste0(dp,"/output/")
+
+### ficheiros do projecto ###
+
+# ficheiro com os dados originais - fd
+fd <- paste0(dd,list.files(dd))
+fd <- read_excel(fd)
